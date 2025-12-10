6c18461672a15d2c69c211c6f6da74325d23ec32
mustofa adny <adniadni04@gmail.com>
2025-12-10 11:42:58 +0700

    setup mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://adniadni04_db_user:adniadni04_db_user@cluster0.z2nxrca.mongodb.net/socials_db?appName=Cluster0',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
