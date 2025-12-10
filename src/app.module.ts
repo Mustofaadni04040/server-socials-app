2308bf948cc788b611e9d457ba3b5a33e1513329
mustofa adny <adniadni04@gmail.com>
2025-12-10 12:10:23 +0700

    feat: generate schema for user using cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI!)],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
