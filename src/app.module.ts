7cce076a2de1b0e38cabe68ab1b34e7a7bc1da76
mustofa adny <adniadni04@gmail.com>
2025-12-10 11:45:46 +0700

    update: mongo url

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://adniadni04_db_user:adniadni04_db_user@cluster0.z2nxrca.mongodb.net/socials_db?appName=Cluster0',
-    ),
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI!)],
   controllers: [AppController],
   providers: [AppService],
 })
