5af128a190e73e06e13167630aca1b92d8216ed5
mustofa adny <adniadni04@gmail.com>
2025-12-19 16:43:37 +0700

    create validation auth dto

--- src/auth/dto/sign-up.dto.ts
+++ src/auth/dto/sign-up.dto.ts
@@ -1,5 +1,10 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class SignUpDto {
+  @IsNotEmpty({ message: 'Name is required' })
   name: string;
+  @IsEmail()
   email: string;
+  @IsNotEmpty({ message: 'Password is required' })
   password: string;
 }
