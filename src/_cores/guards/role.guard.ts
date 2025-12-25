ee6237f597e9e0961ab337f2f8888cddaed0fd5b
mustofa adny <adniadni04@gmail.com>
2025-12-25 14:28:02 +0700

    update: role guard 2

--- src/_cores/guards/role.guard.ts
+++ src/_cores/guards/role.guard.ts
@@ -1,4 +1,9 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { ROLES_KEY } from '../decorators/role.decorator';
@@ -10,13 +15,37 @@ export class RoleGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
+    const currentUser = request.currentUser as IUserPayload;
     const requiredRoles = this.reflector.get<string[]>(
       ROLES_KEY,
       context.getHandler(),
-    );
+    ) as IRole[];
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    if (requiredRoles.length === 0) {
+      return true;
+    }
+
+    if (requiredRoles.includes('admin') && currentUser.role === 'admin') {
+      return true;
+    }
+
+    if (requiredRoles.includes('user') && currentUser.role === 'user') {
+      const userId = currentUser._id;
+      const resourceId = request.params.id as string;
+
+      if (userId === resourceId) {
+        return true;
+      }
+
+      throw new ForbiddenException('Forbidden to access another user resource');
+    }
 
     console.log(requiredRoles);
 
-    return true;
+    throw new ForbiddenException("You don't have permission to access");
   }
 }
