8795a92142977c0ee37e6b8d9d08ffb4e4e52da4
mustofa adny <adniadni04@gmail.com>
2025-12-25 13:47:37 +0700

    feat: role decorator & role guard 1

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,9 +15,11 @@ import { AuthGuard } from 'src/_cores/guards/auth.guard';
 import { CurrentUser } from 'src/_cores/decorators/current-user.decorator';
 import { TransformDTO } from 'src/_cores/interceptors/transform-dto.interceptor';
 import { ResponseUserDto } from './dto/response-user.dto';
+import { RoleGuard } from 'src/_cores/guards/role.guard';
+import { Roles } from 'src/_cores/decorators/role.decorator';
 
 @Controller('users')
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RoleGuard)
 @TransformDTO(ResponseUserDto)
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -28,21 +30,25 @@ export class UserController {
     return currentUser;
   }
 
+  @Roles('admin')
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  @Roles('admin')
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
+  @Roles('admin', 'user')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
+  @Roles('admin', 'user')
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
